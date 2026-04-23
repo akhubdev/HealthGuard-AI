93ba1dd103887052c71abdffda965ec5c85a0d99
Akash Das <cchiku123456@gmail.com>
2026-04-23 16:44:30 +0530

    fix deployment port

--- app.py
+++ app.py
@@ -507,4 +507,5 @@ def download_report(risk, percentage, name):
 # ---------------- RUN SERVER ----------------
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
